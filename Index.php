d5bf431e60ca9d7173e0de5aa3cc71996b5a8325
Loan Gheude <lolo-34@live.fr>
2021-03-01 15:47:10 +0100

    Modification Index

--- Index.php
+++ Index.php
@@ -18,9 +18,10 @@ type="text/css" media="screen" />
 	<p class="Explications"> 
 		Pour utiliser notre application il faut que vous créez un compte en utilisant le bouton "s'incrire" en haut à droite et ou que vous vous connectiez avec le bouton "se connecter" si vous possédez déjà un compte. </p>
 		<p class="Explications"> Vous devez récupérer votre historique d'arènes en jeu sous forme .csv et l'importer dans notre application.</p>
-		<p class="Explications"> Une fois votre historique importé, notre application va analyser vos parties afin de déterminer des statistiques afin que vous puissiez progresser.</p>
-		<p class="Explications"> Les statistiques analysées en arêne côtée sont : votre pourcentage de victoire, votre total de points de côte sur les X dernières parties, les classes contre et avec lesquelles vous gagnez et perdez le plus.
-		Ces statistiques peuvent être triée en fonction de votre spécialization et en 2v2, 3v3 ou pour les deux modes. 
+		<p class="Explications"> Une fois votre historique importé, notre application va analyser vos parties afin de déterminer des statistiques pour que vous puissiez progresser.</p>
+		<p class="Explications"> Les statistiques analysées en arêne côtée sont : votre pourcentage de victoire, votre total de points de côte sur les X dernières parties, les classes contre et avec lesquelles vous gagnez et perdez le plus. </p>
+
+		<p class="Explications"> Ces statistiques peuvent être triée en fonction de votre spécialization et en 2v2, 3v3 ou pour les deux modes. 
 	</p>
 
 <p class="contact"><a href="contact/contact.html">Contact</a></p>
