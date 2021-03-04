ebb4ecbd4be8577afdfcfd4f3841482c1d19a545
Arthur2801 <arthur2801@gmail.com>
2021-03-04 16:53:32 +0100

    Ajout connexion.php

---
+++ connexion.php
@@ -0,0 +1,20 @@
+<html>
+
+	<head>
+	
+		<meta http-equiv="Content-Type"content="text/html; charset=UTF-8" />
+		<title>WOWARENA</title>
+		<link rel="stylesheet" href="styles\style1.css"type="text/css" media="screen" />
+	
+	</head>
+
+	<body>
+		<form action="enregistrement.php" method="post" autocomplete="off">
+			<p>Username :<input type="text" name="u" value="<?php echo $_GET['u'];?>"/></p>
+			<p>Mot de passe :<input type="password" name="mdp1" value=""/></p>
+			<p><input type="submit" value="Envoyer"></p>
+		</form>
+
+	</body>
+
+</html>
