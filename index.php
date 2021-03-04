7cde0fea343dfae6fbd052dcc51901c1b9261e6d
Arthur2801 <arthurbe2801@gmail.com>
2021-03-04 17:51:10 +0100

    Modif index.php

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
-<!Index php>
 <html>
 
  <head>
  <meta http-equiv="Content-Type"content="text/html; charset=UTF-8" />
- <link rel="stylesheet" href="styles/style1.css"
-type="text/css" media="screen" />
+ <link rel="stylesheet" href="styles/style1.css"type="text/css" media="screen" />
 		<title>WoW Arena</title>
  </head>
  <body>
