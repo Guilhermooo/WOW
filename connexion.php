85bdc570f3d005c7b5849ff668d2bc8ed6abffc5
Arthur <47379655+Arthur2801@users.noreply.github.com>
2021-03-04 18:03:45 +0100

    Ajout des pages d'enregistrements des utilisateurs

---
+++ connexion.php
@@ -0,0 +1,20 @@
+<html>
+
+	<head>
+	
+		<meta http-equiv="Content-Type"content="text/html; charset=UTF-8" />
+		<title>WOWARENA</title>
+		<link rel="stylesheet" href="styles\style1.css"type="text/css" media="screen" />
+	
+	</head>
+
+	<body>
+		<form action="enregistrement.php" method="post" autocomplete="off">
+			<p>Username :<input type="text" name="uc" value="<?php echo $_GET['uc'];?>"/></p>
+			<p>Mot de passe :<input type="password" name="mdpc" value=""/></p>
+			<p><input type="submit" value="Envoyer"></p>
+		</form>
+
+	</body>
+
+</html>
