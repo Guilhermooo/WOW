a37e0beff2a6a67cbf64a917cb7dabcde3973b26
Arthur2801 <arthurbe2801@gmail.com>
2021-03-08 14:32:06 +0100

    Ajout deconnexion.php

---
+++ deconnexion.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN""http://www.w3.org/TR/html4/strict.dtd">
+
+<?php
+session_start();
+session_destroy();
+?>
+
+<html>
+
+	<head>
+	
+		<meta http-equiv="Content-Type"content="text/html; charset=UTF-8" />
+		<title>WOW Arena</title>
+		<link rel="stylesheet" href="styles\style1.css"type="text/css" media="screen" />
+		<meta http-equiv="refresh" content="0; URL=index.php">
+	
+	</head>
+
+	
+
+</html>
