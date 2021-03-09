aaa217704c0ffdd338c95556f360e41c09454bee
Arthur2801 <arthurbe2801@gmail.com>
2021-03-09 19:38:19 +0100

    Modif index.php

--- index.php
+++ index.php
@@ -2,45 +2,49 @@
 <?php session_start(); ?>
 <html>
 
- <head>
- 		<meta http-equiv="Content-Type"content="text/html; charset=UTF-8" />
- 		<link rel="stylesheet" href="styles/style1.css"type="text/css" media="screen" />
+	<head>
+
+		<meta http-equiv="Content-Type"content="text/html; charset=UTF-8" />
+		<link rel="stylesheet" href="styles/style1.css"type="text/css" media="screen" />
 		<title>WoW Arena</title>
+		
+	</head>
+
+	<body>
+
 		<?php
-            include ("bd.php");
-            $bdd = getBD();
-        ?>
- </head>
- <body>
-	<h1 class="index">WoW Arena</h1>
+			include ("bd.php");
+			$bdd = getBD();
+		?>
 
-	<?php if(isset($_SESSION['utilisateur'])){
-			echo "Bonjour M.";
-			for($i = 0 ; $i < count($_SESSION['utilisateur']) ; $i++) {
-				echo $_SESSION['client'][$i]." ";
-			}	
-			echo "<br />";?>
-			<a href="deconnexion.php" > Déconnexion</a>
-		<?php }else{?>
-				<h2><a href="inscription.php" >Sign Up</a></h2>
-				<?php echo '<br />'?>
-				<h2><a href="connexion.php" > Log In</a></h2>
-			<?php } ?>
+		<h1 class="index">WoW Arena</h1>
 
-	<h3>Intro</h3>
-	<p class="Intro"> 
-		Welcome to our WoW Arena website, here you can import your players versus players game history from the addon Reflex and analyse your results.
-	</p>
-	</br>
-		<p class="Explications"> See what hold you back from getting your dream rating and optimize your gameplay with our advices on how to counter each class specialization.</p>
-	</br>
-		<p class="Explications"> Get defined statistics from your games to track your performances with your winrate, rating earned or loss and the compositions your are losing against.</p>
-	</br>
-		<p class="Explications"> Those statistics can be sorted the number of games you wish, your specialization and 2v2 or 3v3.</p>
+		<?php if(isset($_SESSION['utilisateur'])){
+				echo "Welcome ". $_SESSION['utilisateur'][0];
+				echo "<br />";?>
+				<a href="deconnexion.php" > Log Out</a>
+			<?php }else{?>
+					<h2><a href="inscription.php" >Sign Up</a></h2>
+					<?php echo '<br />'?>
+					<h2><a href="connexion.php" > Log In</a></h2>
+				<?php } ?>
 
-	<h4>How to import</h4>
-	<p class="Import"> After loging in, you may follow our tutorial on how to import your game history and analyse your results.
+		<h3>Intro</h3>
+		<p class="Intro"> 
+			Welcome to our WoW Arena website, here you can import your players versus players game history from the addon Reflex and analyse your results.
+		</p>
+		</br>
+			<p class="Explications"> See what hold you back from getting your dream rating and optimize your gameplay with our advices on how to counter each class specialization.</p>
+		</br>
+			<p class="Explications"> Get defined statistics from your games to track your performances with your winrate, rating earned or loss and the compositions your are losing against.</p>
+		</br>
+			<p class="Explications"> Those statistics can be sorted the number of games you wish, your specialization and 2v2 or 3v3.</p>
 
-<p class="contact"><a href="contact/contact.html">Contact us</a></p>
- </body>
+		<h4>How to import</h4>
+		<p class="Import"> After loging in, you may follow our tutorial on how to import your game history and analyse your results.
+
+	<p class="contact"><a href="contact/contact.html">Contact us</a></p>
+
+	</body>
+
 </html>
