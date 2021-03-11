4d2853362b9f1b6461ac35ed37c6dd4160bbdb24
Arthur2801 <arthurbe2801@gmail.com>
2021-03-11 17:31:34 +0100

    Ajout import.php

--- import.php
+++ import.php
@@ -1,26 +1,27 @@
 <!DOCTYPE html>
 <html>
 
- 	<head>
- <meta http-equiv="Content-Type"content="text/html; charset=UTF-8" />
- <link rel="stylesheet" href="styles/style1.css"type="text/css" media="screen" />
+	<head>
+
+		<meta http-equiv="Content-Type"content="text/html; charset=UTF-8" />
+		<link rel="stylesheet" href="styles/style1.css"type="text/css" media="screen" />
 		<title>WoW Arena</title>
- </head>
- <body>
-	<h1 class="index">Bonjour, bienvenue sur le site WoW Arena</h1>
-	<h2><a href="inscription.php">S'inscrire</a></h2>
-	<h2><a href="Connection.php">Se connecter</a></h2>
 
-	<h3> Comment ça marche ? </h3>
-	<p class="Explications"> 
-		Pour utiliser notre application il faut que vous créez un compte en utilisant le bouton "s'incrire" en haut à droite et ou que vous vous connectiez avec le bouton "se connecter" si vous possédez déjà un compte. </p>
-		<p class="Explications"> Vous devez récupérer votre historique d'arènes en jeu sous forme .csv et l'importer dans notre application.</p>
-		<p class="Explications"> Une fois votre historique importé, notre application va analyser vos parties afin de déterminer des statistiques pour que vous puissiez progresser.</p>
-		<p class="Explications"> Les statistiques analysées en arêne côtée sont : votre pourcentage de victoire, votre total de points de côte sur les X dernières parties, les classes contre et avec lesquelles vous gagnez et perdez le plus. </p>
+	</head>
 
-		<p class="Explications"> Ces statistiques peuvent être triée en fonction de votre spécialization et en 2v2, 3v3 ou pour les deux modes. 
-	</p>
+	<body>
 
-<p class="contact"><a href="contact/contact.html">Contact</a></p>
- </body>
+		<form enctype="multipart/form-data" action="fileupload.php" method="post">
+
+			<input type="hidden" name="MAX_FILE_SIZE" value="1000000" />
+			<p> Import your csv <input type="file" name="monfichier" /></p>
+			<p><input type="submit" /></p>
+
+		</form>
+
+	</body>
+
+
+	</body>
+
 </html>
