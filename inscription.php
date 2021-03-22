1e06cbf0fd2d2417b220126e53dc925df9325433
Guilhermooo <Guilhermo38@gmail.com>
2021-03-22 15:39:46 +0100

    petit + css

--- inscription.php
+++ inscription.php
@@ -11,15 +11,28 @@
 
 	<body>
 		<div id="header">
- <a href="index.php"><img id="logo" src=images/logoW.png alt="WoW Arena"></a>
-</div>
+		<a href="index.php"><img id="logo" src=images/logoW.png alt="WoW Arena"></a>
+		</div>
+		
+		<div id="global">
+		<h1 class="index">WoW Arena</h1> 
+		<h1 class="index">Sign up : </h1>
+
+		
 		<form action="enregistrement.php" method="post" autocomplete="off">
 			<p>Username :<input type="text" name="u" value="<?php echo $_GET['u'];?>"/></p>
 			<p>Mot de passe :<input type="password" name="mdp1" value=""/></p>
 			<p>Confirmer votre mot de passe :<input type="password" name="mdp2" value=""/></p>
 			<p><input type="submit" value="Envoyer"></p>
 		</form>
-
+		
+		<a href="index.php">
+		Return
+		</a> <br />
+		
+		<p> <br /> </p>
+		</div>
+		
 	</body>
 
 </html>
