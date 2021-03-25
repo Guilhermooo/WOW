1fb2e7d96ef4cafc8815c6ee1523a19fb1f3bc89
Guilhermooo <Guilhermo38@gmail.com>
2021-03-25 17:48:43 +0100

    modif css

--- index.php
+++ index.php
@@ -17,39 +17,37 @@
 		?>
 <body>
 <div id="header">
- <a href="index.php"><img id="logo" src=images/logoW.png alt="WoW Arena"></a>
+	<a href="index.php"><img id="logo" src=images/logoW.png alt="WoW Arena"></a>
+	
+	<div id="text">
+	<?php if(isset($_SESSION['utilisateur'])){
+		echo "Welcome ". $_SESSION['utilisateur'][0];
+		echo "<br />";?>
+		<h1 class="bouton"> •<a href="deconnexion.php">Log Out</a>
+		•<a href="import.php">Import</a>
+		•<a href="statistics.php" >Statistics</a></h1>
+	<?php }else{?>
+		<h1 class="bouton"> •<a href="inscription.php">Sign Up</a>
+		•<a href="connexion.php">Log In</a> </h1>
+		<?php } ?>
+		
+	</div>
+	
 </div>
 
 <div id="global">
-	<h1>WoW Arena</h1>
-
-		<?php if(isset($_SESSION['utilisateur'])){
-				echo "Welcome ". $_SESSION['utilisateur'][0];
-				echo "<br />";?>
-				<h2><a href="deconnexion.php">Log Out</a></h2>
-				<?php echo '<br />'?>
-				<h2><a href="import.php" >Import</a></h2>
-				<h2><a href="statistics.php" >Statistics</a></h2>
-			<?php }else{?>
-					<h2><a href="inscription.php">Sign Up</a></h2>
-					<?php echo '<br/>'?>
-					<h2><a href="connexion.php">Log In</a></h2>
-				<?php } ?>
-
-		<h3>Intro</h3>
+	<h1 class="title">WoW Arena</h1>
+		<h3 class="bouton"> <u> Introduction </u> </h3>
 		<p class="intro"> 
 			Welcome to our WoW Arena website, here you can import your players versus players game history from the addon Reflex and analyse your results. </p>
-		</br>
 			<p class="explications"> See what hold you back from getting your dream rating and optimize your gameplay with our advices on how to counter each class specialization.</p>
-		</br>
 			<p class="explications"> Get defined statistics from your games to track your performances with your winrate, rating earned or loss and the compositions your are losing against.</p>
-		</br>
 			<p class="explications"> Those statistics can be sorted the number of games you wish, your specialization and 2v2 or 3v3.</p>
 
-		<h4 class="import">How to import</h4>
+		<h4 class="import">How to import : </h4>
 		<p class="import"> After loging in, you may follow our tutorial on how to import your game history and analyse your results.</p>
 
-	<p class="contact"><a href="contact/contact.php">Contact us</a></p>
+	<p class="bouton"><a href="contact/contact.php">Contact us</a></p>
 
 	</body>
 </div>
