2add759fffceb98fe25c1ac3a3161ef613b7444a
Guilhermooo <Guilhermo38@gmail.com>
2021-03-25 17:49:51 +0100

    modif css

--- contact/contact.php
+++ contact/contact.php
@@ -14,7 +14,7 @@
 </div>
 
 	<div id="global">
-		<h1 class="index">WoW Arena</h1>
+		<h1 class="title">WoW Arena</h1>
 
 		<?php if(isset($_SESSION['utilisateur'])){
 				echo "Welcome ". $_SESSION['utilisateur'][0];
@@ -28,7 +28,7 @@
 				<h2><a href="../connexion.php">Log In</a></h2>
 		<?php } ?>
 				
-		<p class="contact"> Here are the directions to contact us if you need help : </p> <br/>
+		<p class="contact"> Here are the directions to contact us if you need help : </p>
 		
 		<p>
 			<ul>
@@ -37,19 +37,16 @@
 				<li> Bellot Arthur : </li>
 				<li> Arthurbe2801@gmail.com </li> <br />
 				<li> Gheude Loan : </li>
-				<li> Loangheude@gmail.com </li> <br/> 
-				</ul>
-		</p> <br /> 
+				<li> Loan.gheude@gmail.com </li> <br/> 
+			</ul>
+		</p> 
 		
 		<p class="contact"> Do not hesitate to make suggestions in order to improve our site. </p>
 		
-		<p> <em> Thank you for your visit </em> </p> <br />
+		<p> <em> Thank you for your visit </em> </p>
 		
-		<a href="../index.php">
-		Return
-		</a> <br />
+		<p class="bouton"> <a href="../index.php"> Return </a> </p>
 		
-		<p> <br /> </p>
 		</div>
 	</body>
 </html>
