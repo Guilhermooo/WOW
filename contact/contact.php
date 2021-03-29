5ac2f61363f7bd143bedb5e79646ebd941f94792
Guilhermooo <Guilhermo38@gmail.com>
2021-03-29 14:50:58 +0200

    modif css

--- contact/contact.php
+++ contact/contact.php
@@ -7,38 +7,38 @@
 		<link rel="stylesheet" href="../styles/style1.css" type="text/css"media="screen" />
 		<title>WoW Arena</title>
 	</head>
-	
+		
 	<body>
-<div id="header">
- <a href="index.php"><img id="logo" src=../images/logoW.png alt="WoW Arena"></a>
-</div>
-
+		<div id="header">
+			<a href="index.php"><img id="logo" src=../images/logoW.png alt="WoW Arena"></a>
+	
+		<div id="text">
+		<h3 class="bouton">
+			<?php if(isset($_SESSION['utilisateur'])){
+				echo "Welcome ". $_SESSION['utilisateur'][0]."";?> </h3>
+				<h1 class="bouton"> •<a href="../deconnexion.php">Log Out</a>
+				•<a href="../import.php">Import</a>
+				•<a href="../statistics.php" >Statistics</a></h1>
+			<?php }else{?>
+				<h1 class="bouton"> •<a href="../inscription.php">Sign Up</a>
+				•<a href="../connexion.php">Log In</a> </h1>
+				<?php } ?>
+		
+		</div>
+	
+		</div>
 	<div id="global">
 		<h1 class="title">WoW Arena</h1>
-
-		<?php if(isset($_SESSION['utilisateur'])){
-				echo "Welcome ". $_SESSION['utilisateur'][0];
-				echo "<br />";?>
-				<a href="../deconnexion.php">Log Out</a>
-				<?php echo '<br />'?>
-				<a href="../import.php" >Import</a>
-		<?php }else{?>
-				<h2><a href="../inscription.php">Sign Up</a></h2>
-				<?php echo '<br/>'?>
-				<h2><a href="../connexion.php">Log In</a></h2>
-		<?php } ?>
 				
 		<p class="contact"> Here are the directions to contact us if you need help : </p>
 		
 		<p>
-			<ul>
 				<li> Miramond Guillaume : </li>
-				<li> Guilhermo38@gmail.com </li> <br />
+				<p class="mail"> Guilhermo38@gmail.com </p> <br />
 				<li> Bellot Arthur : </li>
-				<li> Arthurbe2801@gmail.com </li> <br />
+				<p class="mail"> Arthurbe2801@gmail.com </p> <br />
 				<li> Gheude Loan : </li>
-				<li> Loan.gheude@gmail.com </li> <br/> 
-			</ul>
+				<p class="mail"> Loan.gheude@gmail.com </p> <br/> 
 		</p> 
 		
 		<p class="contact"> Do not hesitate to make suggestions in order to improve our site. </p>
