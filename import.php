56d435ee31f4f91ab9aa6c6b55d5629bd51f712f
Guilhermooo <Guilhermo38@gmail.com>
2021-04-01 17:26:40 +0200

    update css

--- import.php
+++ import.php
@@ -15,7 +15,7 @@
 		</div>
 		
 		<div id="global">
-		<h1 class="index">WoW Arena</h1>
+		<h1 class="title">WoW Arena</h1>
 		<h1 class="index"><u> Import : </u></h1>
 
 		<form enctype="multipart/form-data" action="fileupload.php" method="post">
